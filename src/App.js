74088c2b236fac3ed15c9e664c2c04def035d196
Kingsley Solomon <kinxly@gmail.com>
2022-09-03 09:02:18 +0100

    [clearn up]

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
   return (
     <>
       <Toaster />
-      <div>
+      {/* <div> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path ="/license-verification" element={<Form />} />
           <Route path ="/face-verification" element={<FormFace />} />
         </Routes>
-      </div>
+      {/* </div> */}
     </>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,24 +2,12 @@ import React from 'react';
 import { Link  } from "react-router-dom";
 
 const Home = () => {
-
-    // const [verification, setVerification] = useState('');
-    // const [value, setValue] = useState('');
-
-    // const handleChange = (e) => {
-    //   setValue(e.target.value);
-    // };
-    
   return (
     <div className='form-container home'>
         <h1>Choose Verification Action</h1>
-        {/* <select id="verification" value={value} onChange={handleChange} >
-            <option value="PlateNumber" >Plate Number</option>
-            <option value="FaceVerification" >Face Verification</option>
-        </select>
-        <Link onClick={console.log(value, 'value')} to ={value === "PlateNumber" ? '/license-verification': '/face-verification'} >Continue</Link> */}
-        <Link to = '/face-verification'>Face Verification</Link>
-        <Link to= '/license-verification'>License Verification</Link>
+
+        <Link className='nav' to = '/face-verification'>Face Verification</Link>
+        <Link className='nav' to= '/license-verification'>License Verification</Link>
     </div>
   )
   }
